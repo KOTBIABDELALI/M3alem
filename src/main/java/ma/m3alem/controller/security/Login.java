d92bcd192e5e6d8876847ddb55c81dc2b21bdb5e
aelkhazzar <aelkhazzar@sqli.com>
2019-04-10 16:07:47 +0100

    fix-002-fixing the Css problem +  changing some pages .

--- src/main/java/ma/m3alem/controller/security/Login.java
+++ src/main/java/ma/m3alem/controller/security/Login.java
@@ -1,7 +1,9 @@
 package ma.m3alem.controller.security;
 
+import ma.m3alem.entities.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,13 +11,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 
 public class Login {
-    @RequestMapping(name = "/login" ,method = RequestMethod.POST)
-    public String doLogin(@RequestParam String name, @RequestParam(value = "pass") String password, Model model){
-        if (password.compareTo("nimda")==0){
-            model.addAttribute("message","hello "+ name);
+    @RequestMapping(name = "/loginAction" ,method = RequestMethod.POST)
+    public String doLogin(@ModelAttribute("user") User user , Model model){
+        if (user.getPassword().compareTo("nimda")==0){
+            model.addAttribute("message","hello "+ user.getLastName());
             return "viewpage1";
         }
-        model.addAttribute("messageErr","ouups " +name+ " : incorrect password");
+        model.addAttribute("messageErr","ouups " + user.getLastName()+ " : incorrect password");
         return "viewpage2";
     }
 }
